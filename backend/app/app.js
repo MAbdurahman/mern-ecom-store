7866498734f9af72e8844054861823593939ab63
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-11 15:13:42 -0600

    Complete basic setup of backend/app/app.js and backend/pages/homePage.js

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,6 +1,7 @@
 /************************* imports *************************/
 import colors from 'colors';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 import helmet from 'helmet';
@@ -18,9 +19,23 @@ colors.enabled = true;
 if (process.env.NODE_ENV === 'development') {
    app.use(morgan('dev'));
 }
+app.use(
+   cors({
+      origin: "http://localhost:5173",
+      methods: ["GET", "POST", "DELETE", "PUT"],
+      allowedHeaders: [
+         "Content-Type",
+         "Authorization",
+         "Cache-Control",
+         "Expires",
+         "Pragma",
+      ],
+      credentials: true,
+   })
+);
+app.use(cookieParser());
 app.use(express.json()); // for parsing application/json
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-app.use(cookieParser());
 app.use(helmet());
 
 /************************* import all routes *************************/
--- backend/pages/homePage.js
+++ backend/pages/homePage.js
@@ -11,6 +11,7 @@ export default function HomePage() {
         <!--favicon -->
         <link rel="icon" type="image/png"
               href="https://res.cloudinary.com/mdbdrrhm/image/upload/v1699189975/favicons/favicon_jouvmk.png"/>
+              
         <!--normalize styles -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
               integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
