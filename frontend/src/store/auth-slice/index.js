f9d46667763c8d771c77a2778b75269135284c0a
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-11 15:41:47 -0600

    Add BrowserRouter and redux store to main.jsx

---
+++ frontend/src/store/auth-slice/index.js
@@ -0,0 +1,33 @@
+
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+   isAuthenticated: false,
+   isLoading: false,
+   user: null,
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const authSlice = createSlice({
+   name: 'auth',
+   initialState,
+   reducers: {
+      setUser: (state, action) => {},
+   },
+});
+
+export const { setUser } = authSlice.actions;
+export default authSlice.reducer;
