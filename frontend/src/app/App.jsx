bf93095cea5d7175eae52e1ddc08e5ee789480f2
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-12 05:13:04 -0600

    Add frontend/src/context/notificationContext.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -2,7 +2,7 @@ export default function App() {
 
    return (
       <div>
-         <h2 className='text-3xl text-center font-head font-bold'>mern-ecom-store</h2>
+         <h2 className='text-3xl text-center text-red-800 font-body font-bold'>mern-ecom-store</h2>
       </div>
 
    );
