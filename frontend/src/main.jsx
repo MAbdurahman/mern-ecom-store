50e097cda32707b7ee11e4d42a11e52c1fbacd61
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-12 05:43:06 -0600

    Add frontend/src/context/notificationContext.jsx and semantic colors to src/index.css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,13 +2,16 @@ import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './app/App.jsx';
 import {BrowserRouter} from 'react-router-dom';
+import NotificationProvider from './context/notificationContext.jsx';
 import {Provider} from 'react-redux';
 import store from './store/store.js';
 
 createRoot(document.getElementById('root')).render(
    <BrowserRouter>
       <Provider store={store}>
-         <App/>
+         <NotificationProvider>
+            <App/>
+         </NotificationProvider>
       </Provider>
    </BrowserRouter>
 );
