73af75bd14d8066c802367454228f5076ba50f1e
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-14 07:25:39 -0600

    Add basic configuration for AccessValidator.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from 'react-router-dom';
 import AuthLayout from '../components/auth/AuthLayout.jsx';
 import SignIn from '../pages/auth/SignIn.jsx';
 import SignUp from '../pages/auth/SignUp.jsx';
@@ -17,36 +17,50 @@ import UserPaymentSuccess from '../pages/user/UserPaymentSuccess.jsx';
 import UserProductSearch from '../pages/user/UserProductSearch.jsx';
 import NotFoundError from '../pages/error/NotFoundError.jsx';
 import UnauthorizedError from '../pages/error/UnauthorizedError.jsx';
-
+import AccessValidator from '../components/common/AccessValidator.jsx';
 
 
 export default function App() {
+   const isAuthenticated = true;
+   const user = {
+      name: 'John Doe',
+      role: 'user'
+   };
 
    return (
       <div className="flex flex-col overflow-hidden bg-white">
-         <h1>Header Component</h1>
+         {/*<h1>Header Component</h1>*/}
          <Routes>
-            <Route path='/auth' element={<AuthLayout/>}>
-               <Route path='sign-in' element={<SignIn/>} />
-               <Route path='sign-up' element={<SignUp/>} />
+            <Route path="/auth" element={
+               <AccessValidator isAuthenticated={isAuthenticated} user={user}>
+                  <AuthLayout/>
+               </AccessValidator>}>
+               <Route path="sign-in" element={<SignIn/>}/>
+               <Route path="sign-up" element={<SignUp/>}/>
             </Route>
-            <Route path={'/admin'} element={<AdminLayout/>}>
-               <Route path='dashboard' element={<AdminDashboard/>} />
-               <Route path='products' element={<AdminProducts />} />
-               <Route path='orders' element={<AdminOrders />} />
-               <Route path='features' element={<AdminFeatures />} />
+            <Route path={'/admin'} element={
+               <AccessValidator isAuthenticated={isAuthenticated} user={user}>
+                  <AdminLayout/>
+               </AccessValidator>}>
+               <Route path="dashboard" element={<AdminDashboard/>}/>
+               <Route path="products" element={<AdminProducts/>}/>
+               <Route path="orders" element={<AdminOrders/>}/>
+               <Route path="features" element={<AdminFeatures/>}/>
             </Route>
-            <Route path='user' element={<UserLayout/>} >
-               <Route path='home' element={<UserHomePage/>} />
-               <Route path='listing' element={<UserProductsListing/>} />
-               <Route path='checkout' element={<UserCheckout/>} />
-               <Route path='account' element={<UserAccount/>} />
-               <Route path='paypal-return' element={<UserPaypalReturn/>} />
-               <Route path='payment-success' element={<UserPaymentSuccess/>} />
-               <Route path='search' element={<UserProductSearch/>} />
+            <Route path="user" element={
+               <AccessValidator isAuthenticated={isAuthenticated} user={user}>
+                  <UserLayout/>
+               </AccessValidator>}>
+               <Route path="home" element={<UserHomePage/>}/>
+               <Route path="listing" element={<UserProductsListing/>}/>
+               <Route path="checkout" element={<UserCheckout/>}/>
+               <Route path="account" element={<UserAccount/>}/>
+               <Route path="paypal-return" element={<UserPaypalReturn/>}/>
+               <Route path="payment-success" element={<UserPaymentSuccess/>}/>
+               <Route path="search" element={<UserProductSearch/>}/>
             </Route>
-            <Route path={'/unauthorized'} element={<UnauthorizedError/>} />
-            <Route path='*' element={<NotFoundError/>} />
+            <Route path={'/unauthorized'} element={<UnauthorizedError/>}/>
+            <Route path="*" element={<NotFoundError/>}/>
 
          </Routes>
 
--- frontend/src/components/common/AccessValidator.jsx
+++ frontend/src/components/common/AccessValidator.jsx
@@ -1,9 +1,54 @@
-export default function AccessValidator() {
+import {Navigate, useLocation} from 'react-router-dom';
+import {Fragment} from 'react';
+
+
+export default function AccessValidator({isAuthenticated, user, children }) {
+   const location = useLocation();
+
+   console.log(location.pathname, isAuthenticated);
+
+   if (location.pathname === '/') {
+      if (!isAuthenticated) {
+         return <Navigate to='/auth/sign-in' />;
+      } else {
+         if (user?.role === 'admin') {
+            return <Navigate to='/admin/dashboard' />;
+         } else {
+            return <Navigate to='/user/home' />;
+         }
+      }
+   }
+
+   if (!isAuthenticated &&
+      !(location.pathname.includes('/sign-in') || location.pathname.includes('/sign-up'))
+   ) {
+      return <Navigate to='/auth/sign-in' />;
+   }
+
+   if (isAuthenticated && (location.pathname.includes('/sign-in') ||
+      location.pathname.includes('/sign-up'))) {
+
+      if (user?.role === 'admin') {
+         return <Navigate to='/admin/dashboard' />;
+
+      } else {
+         return <Navigate to='/user/home' />;
+
+      }
+   }
+
+   if (isAuthenticated && user?.role !== 'admin' && location.pathname.includes('admin')) {
+      return <Navigate to='/unauthorized' />;
+   }
+
+   if (isAuthenticated && user?.role === 'admin' && location.pathname.includes('user')) {
+      return <Navigate to='/admin/dashboard' />;
+   }
 
    return (
-      <div>
-         <h2>AccessValidator</h2>
-      </div>
+      <Fragment>
+         {children}
+      </Fragment>
 
    );
 }
