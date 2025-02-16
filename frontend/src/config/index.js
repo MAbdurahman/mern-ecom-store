8fd0b1126fe2f9132ec81b8192ab7affbb300968
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-02-15 18:27:10 -0600

    src/components/common/CommonForm.jsx

---
+++ frontend/src/config/index.js
@@ -0,0 +1,41 @@
+
+export const signUpFormControls = [
+   {
+      name: "fullName",
+      label: "Full name",
+      placeholder: "Enter full name",
+      componentType: "input",
+      type: "text",
+   },
+   {
+      name: "email",
+      label: "Email",
+      placeholder: "Enter email",
+      componentType: "input",
+      type: "email",
+   },
+   {
+      name: "password",
+      label: "Password",
+      placeholder: "Enter password",
+      componentType: "input",
+      type: "password",
+   },
+];
+
+export const signInFormControls = [
+   {
+      name: "email",
+      label: "Email",
+      placeholder: "Enter email",
+      componentType: "input",
+      type: "email",
+   },
+   {
+      name: "password",
+      label: "Password",
+      placeholder: "Enter password",
+      componentType: "input",
+      type: "password",
+   },
+];
