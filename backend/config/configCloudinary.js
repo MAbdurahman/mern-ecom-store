f3ecc26be8111ef8f9cb2b0127cb8275fa5dc4e5
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-03-09 03:24:40 -0500

    Add backend/config and its configuration files

---
+++ backend/config/configCloudinary.js
@@ -0,0 +1,23 @@
+import {v2 as cloudinary} from 'cloudinary';
+import multer from 'multer';
+
+export const connectCloudinary = async () => {
+   await cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+   });
+}
+
+
+console.log(cloudinary.config());
+const storage = new multer.memoryStorage();
+
+export async function handleUploadedImage(file) {
+
+   return await cloudinary.uploader.upload(file, {
+      resource_type: 'auto'
+   });
+}
+
+export const upload = multer({ storage });
