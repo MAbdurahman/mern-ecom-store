9d1737cfe55f791e69be97b1a0e6fd68b1746466
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-03-10 08:31:24 -0500

    Add favicon.png for frontend

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -4,8 +4,8 @@ export default function App() {
 
    return (
       <div className="flex flex-col items-center justify-center min-h-svh">
-         <h2>mern-ecom-store</h2>
-         <Button className="uppercase font-semibold mt-1 cursor-pointer">Click me</Button>
+         <h2 className="uppercase mb-2">mern-ecom-store</h2>
+         <Button variant="default" className="font-semibold mt-1 cursor-pointer">click me</Button>
       </div>
 
    );
