c6a3ca24d13701cca1a5829bba6476516ea05d20
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-03-10 08:14:27 -0500

    Configure frontend/ for tailwindcss-v4 and shadcn ui

---
+++ frontend/src/app/App.jsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+export default function App() {
+
+   return (
+      <div className="flex flex-col items-center justify-center min-h-svh">
+         <h2>mern-ecom-store</h2>
+         <Button className="uppercase font-semibold mt-1 cursor-pointer">Click me</Button>
+      </div>
+
+   );
+}
