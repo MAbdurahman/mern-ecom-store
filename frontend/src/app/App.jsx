d042618193f3c3162a492c19ba1caae658052043
Mahdi Abdurrahman <mdbdrrhm@gmail.com>
2025-03-11 02:20:16 -0500

    Add custom fonts and some styles to frontend/src/index.css

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -4,7 +4,7 @@ export default function App() {
 
    return (
       <div className="flex flex-col items-center justify-center min-h-svh">
-         <h2 className="uppercase mb-2">mern-ecom-store</h2>
+         <h2 className="uppercase font-semibold mb-2">mern-ecom-store</h2>
          <Button variant="default" className="font-semibold mt-1 cursor-pointer">click me</Button>
       </div>
 
